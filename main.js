d0a6543436a1ce3fb3d3f9a64b6c8020961c061f
vincaf <vincenzocafassog@gmail.com>
2022-06-30 16:59:56 +0200

    writing post in the feed

--- main.js
+++ main.js
@@ -96,3 +96,46 @@ const posts = [
         "created": "2021-12-11"
     }
 ];
+
+console.log(posts);
+
+drawPost(posts, 'post');
+
+function drawTeam( postsList, htmlContainerClass ){
+    const post = document.getElementsByClassName(htmlContainerClass);
+    post.innerHTML = "";
+
+    for (let i = 0; i < postsList.length; i++) {
+        const currentElement = postsList[i];
+
+        post.innerHTML += `
+        <div class="post__header">
+                <div class="post-meta">
+                    <div class="post-meta__icon">
+                        <img class="profile-pic" src="${currentElement.author.image}" alt="${currentElement.author.name}">
+                    </div>
+                    <div class="post-meta__data">
+                        <div class="post-meta__author">${currentElement.author.name}</div>
+                        <div class="post-meta__time">${currentElement.created}</div>
+                    </div>
+                </div>
+            </div>
+            <div class="post__text">${currentElement.content}</div>
+            <div class="post__image">
+                <img src="${currentElement.media}" alt="">
+            </div>
+            <div class="post__footer">
+                <div class="likes js-likes">
+                    <div class="likes__cta">
+                        <a class="like-button  js-like-button" href="#" data-postid="${currentElement.id}">
+                            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="${currentElement.is_liked}"></i>
+                            <span class="like-button__label">Mi Piace</span>
+                        </a>
+                    </div>
+                    <div class="likes__counter">
+                        Piace a <b id="like-counter-1" class="js-likes-counter">${currentElement.likes}</b> persone
+                    </div>
+                </div>
+            </div>`;
+    }
+}
